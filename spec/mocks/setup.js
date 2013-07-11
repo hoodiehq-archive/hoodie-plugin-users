a430dce8cb4a8e5f3ec83f582c6cd503d076cdad
Gregor Martynus <gregor@martynus.net>
2013-07-11 11:45:38 +0200

    cleanup
    This commit was sponsored by The Hoodie Firm
    with support from NLnet: http://nlnet.nl
    http://go.hood.ie/thehoodiefirm

--- spec/mocks/setup.js
+++
@@ -1,7 +0,0 @@
-var when = require('when');
-var CouchMock = require('./couch');
-
-module.exports = function(config) {
-  this.couch = new CouchMock(config);
-  return when.resolve();
-};
--- spec/mocks/username_change.js
+++
@@ -1,4 +0,0 @@
-var UsernameChangeMock = {
-};
-
-module.exports = UsernameChangeMock;
