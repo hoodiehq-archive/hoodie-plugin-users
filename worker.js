620e67ca0d37f440bd23f162b3f22eb59d876915
Gregor Martynus <gregor@martynus.net>
2013-08-02 20:34:43 +0200

    implemented password reset & username change based on new worker API
    This commit was sponsored by The Hoodie Firm.
    You can hire The Hoodie Firm:
    http://go.hood.ie/thehoodiefirm

--- worker.js
+++ worker.js
@@ -1,35 +1,18 @@
 module.exports = function(hoodie, doneCallback) {
 
   var userAccount = require('./lib/user_account.js')(hoodie);
+  var passwordReset = require('./lib/password_reset.js')(hoodie);
 
   // handle changes in _users
   hoodie.account.on('change:user', userAccount.handleChange)
   hoodie.account.on('change:user_anonymous', userAccount.handleChange)
 
-  // this is how a passwordReset doc looks like (a bit simplified):
   //
-  // {
-  //   _id: "org.couchdb.user:$passwordReset/joe@example.com/uuid789",
-  //   name: "$passwordReset/joe@example.com/uuid789",
-  //   type: 'user',
-  //   roles: []
-  // };
-  //
-  // 1. check if username exists
-  // 2. generate new password
-  // 3. update current user object
-  // 4. send out new password via email
-  // 5. remove $resetPassword object
-  //
+  hoodie.account.on('change:$passwordReset', passwordReset.handleChange)
+
+
   function handlePasswordReset(object) {
     console.log('handlePasswordReset!');
-  }
-
-  // 1. create new user doc with current property and new username
-  // 2. remove old username
-  //
-  function handleUsernameChange(userObject) {
-
   }
 
   doneCallback();
