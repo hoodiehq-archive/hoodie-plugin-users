70d83e8a12915a857a9460342378ec937e20d9e4
Gregor Martynus <gregor@martynus.net>
2013-08-29 18:31:51 +0200

    jshinting the shid out of it
    * * *
    
    This commit was sponsored by local.ch, Switzerland's official
    phonebook directory and kick-ass web & mobile development house.
    
    They are a great place to work in Zurich and they want to hire you:
    
    - Frontend Web Developer: http://go.hood.ie/local-ch-web-developer-gh
    - Mobile Engineer: http://go.hood.ie/local-ch-mobile-web-engineer-gh

--- worker.js
+++ worker.js
@@ -1,19 +1,20 @@
-module.exports = function(hoodie, doneCallback) {
+module.exports = function (hoodie, doneCallback) {
 
   var userAccount = require('./lib/user_account.js')(hoodie);
   var passwordReset = require('./lib/password_reset.js')(hoodie);
 
+  // bootstrap existing users
+  hoodie.account.findAll(function (accounts) {
+    console.log(accounts);
+  });
+
   // handle changes in _users
-  hoodie.account.on('change:user', userAccount.handleChange)
-  hoodie.account.on('change:user_anonymous', userAccount.handleChange)
+  hoodie.account.on('change:user', userAccount.handleChange);
+  hoodie.account.on('change:user_anonymous', userAccount.handleChange);
 
   //
-  hoodie.account.on('change:$passwordReset', passwordReset.handleChange)
+  hoodie.account.on('change:$passwordReset', passwordReset.handleChange);
 
-
-  function handlePasswordReset(object) {
-    console.log('handlePasswordReset!');
-  }
 
   doneCallback();
-}
+};
