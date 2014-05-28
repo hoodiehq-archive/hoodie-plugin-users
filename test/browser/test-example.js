f216dafa730e20ea50e1345638953ca9168865e9
Caolan McMahon <caolan@caolanmcmahon.com>
2014-05-28 13:31:32 +0100

    add basic tests for signUp, signIn and signOut

--- test/browser/test-example.js
+++ test/browser/test-example.js
@@ -1,4 +1,4 @@
-suite('example', function () {
+suite('signup and sessions', function () {
 
   setup(function (done) {
     // phantomjs seems to keep session data between runs,
@@ -9,16 +9,64 @@ suite('example', function () {
     });
   });
 
-  test('say hello', function (done) {
+  test('signUp', function (done) {
     this.timeout(10000);
-    var task = hoodie.hello('world');
-    task.fail(function (err) {
-      assert.ok(false, err.message);
-    });
-    task.done(function (doc) {
-      assert.equal(doc.msg, 'Hello, world');
-      done();
-    });
+    hoodie.account.signUp('testuser', 'password')
+      .fail(function (err) {
+        assert.ok(false, err.message);
+      })
+      .done(function () {
+        assert.equal(
+          hoodie.account.username,
+          'testuser',
+          'should be logged in after signup'
+        );
+        done();
+      });
+  });
+
+  test('signIn', function (done) {
+    this.timeout(10000);
+    assert.ok(!hoodie.account.username, 'start logged out');
+    hoodie.account.signIn('testuser', 'password')
+      .fail(function (err) {
+        assert.ok(false, err.message);
+      })
+      .done(function () {
+        assert.equal(hoodie.account.username, 'testuser');
+        done();
+      })
+  });
+
+  test('signOut', function (done) {
+    this.timeout(10000);
+    hoodie.account.signIn('testuser', 'password')
+      .then(function () {
+        return hoodie.account.signOut();
+      })
+      .fail(function (err) {
+        assert.ok(false, err.message);
+      })
+      .done(function () {
+        assert.ok(!hoodie.account.username, 'should be logged out');
+        done();
+      })
+  });
+
+  test('signUp while logged in should fail', function (done) {
+    this.timeout(10000);
+    hoodie.account.signIn('testuser', 'password')
+      .then(function () {
+        return hoodie.account.signUp('testuser2', 'password');
+      })
+      .fail(function (err) {
+        assert.ok(true, 'signUp should fail');
+        done();
+      })
+      .done(function () {
+        assert.ok(false, 'signUp should not succeed');
+        done();
+      })
   });
 
 });
