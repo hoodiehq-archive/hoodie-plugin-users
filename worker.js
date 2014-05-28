f176461c9c37555d4c65fbcc15c1a861437810af
Caolan McMahon <caolan@caolanmcmahon.com>
2014-05-28 14:51:39 +0100

    started code to create user db and confirm user

--- worker.js
+++ worker.js
@@ -1,14 +1,49 @@
 /**
- * Hoodie plugin template
- * An example plugin worker, this is where you put your backend code (if any)
+ * Users plugin
+ * Manages accounts and user dbs
  */
+
+var async = require('async');
+
 
 module.exports = function (hoodie, callback) {
 
-  // setup task handlers
-  hoodie.task.on('hello:add', function (db, task) {
-    task.msg = 'Hello, ' + task.name;
-    hoodie.task.success(db, task);
+  /**
+   * Adds appropriate roles to user doc to enable user db
+   * access and login ability on the frontend
+   */
+
+  function confirmUser(doc, callback) {
+    var roles = [
+      doc.hoodieId,
+      'confirmed',
+      'hoodie:read:user/' + doc.hoodieId,
+      'hoodie:write:user/' + doc.hoodieId
+    ];
+    hoodie.account.update('user', doc.id, {roles: roles}, callback);
+  }
+
+  /**
+   * Create user's personal database and grants them read/write access
+   */
+
+  function createUserDB(doc, callback) {
+    var name = 'user/' + doc.hoodieId;
+    hoodie.database.add(name, callback);
+  }
+
+  /**
+   * Event handlers
+   */
+
+  hoodie.account.on('user:change', function (doc) {
+    async.applyEachSeries([createUserDB, confirmUser], doc, function (err) {
+      if (err) {
+        console.error('Error confirming user: %s', err);
+        console.error(err);
+        return;
+      }
+    });
   });
 
   // plugin initialization complete
