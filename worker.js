f44fe4f3eb9efe9da00286f18b824664dac97f0b
Caolan McMahon <caolan@caolanmcmahon.com>
2014-07-01 14:59:06 +0100

    remove user db on account.destroy

--- worker.js
+++ worker.js
@@ -4,6 +4,7 @@
  */
 
 var async = require('async');
+var _ = require('lodash');
 
 
 module.exports = function (hoodie, callback) {
@@ -17,10 +18,18 @@ module.exports = function (hoodie, callback) {
     var roles = [
       doc.hoodieId,
       'confirmed',
-      'hoodie:read:user/' + doc.hoodieId,
-      'hoodie:write:user/' + doc.hoodieId
+      'hoodie:read:' + userDB(doc),
+      'hoodie:write:' + userDB(doc)
     ];
     hoodie.account.update('user', doc.id, {roles: roles}, callback);
+  }
+
+  /**
+   * Returns user db name for given user doc
+   */
+
+  function userDB(doc) {
+    return 'user/' + doc.hoodieId;
   }
 
   /**
@@ -28,23 +37,59 @@ module.exports = function (hoodie, callback) {
    */
 
   function createUserDB(doc, callback) {
-    var name = 'user/' + doc.hoodieId;
-    hoodie.database.add(name, callback);
+    hoodie.database.add(userDB(doc), callback);
   }
+
+  /**
+   * Deletes a users's database when their account is destroyed
+   */
+
+  function deleteUserDB(doc, callback) {
+    hoodie.database.remove(userDB(doc), callback);
+  }
+
+  /**
+   * Detects if user has a given role
+   */
+
+  var hasRole = _.curry(function (role, doc) {
+    return _.contains(doc.roles, role);
+  });
+
+  /**
+   * Returns true if user is confirmed, false otherwise
+   */
+
+  var isConfirmed = hasRole('confirmed');
 
   /**
    * Event handlers
    */
 
   hoodie.account.on('user:change', function (doc) {
-    async.applyEachSeries([createUserDB, confirmUser], doc, function (err) {
+    if (doc._deleted) {
+      deleteUserDB(doc, logErrors('Error deleting user db'));
+    }
+    else if (!isConfirmed(doc)) {
+      async.applyEachSeries(
+        [createUserDB, confirmUser], doc, logErrors('Error confirming user')
+      );
+    }
+  });
+
+  /**
+   * Logs errors encountered by event handlers
+   */
+
+  function logErrors(context) {
+    return function (err) {
       if (err) {
-        console.error('Error confirming user: %s', err);
+        console.error(context + ': %s', err);
         console.error(err);
         return;
       }
-    });
-  });
+    };
+  }
 
   // plugin initialization complete
   callback();
