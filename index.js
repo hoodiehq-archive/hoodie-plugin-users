563c705f24ec57484d518ee6c7995e874b45ebcd
Alex Feyerke <espy@users.noreply.github.com>
2015-07-29 15:40:17 +0200

    feat: add CouchDB view generation and remove frontend part
    BREAKING CHANGE: The users plugin no longer contains the frontend part, which is now directly embedded into the "new" ember admin dashboard.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var passwordReset = require('./lib/password_reset');
 var changeUsername = require('./lib/change_username');
 var signUp = require('./lib/signup');
 var utils = require('./lib/utils');
-
+var async = require('async');
 
 var sources = [];
 
@@ -79,9 +79,83 @@ module.exports = function (hoodie, callback) {
     }
   });
 
+  async.applyEachSeries([
+    exports.create_user_index,
+    exports.create_user_index_by_created_at
+  ], hoodie, function(error) {
+    if(error){
+      return callback(error);
+    } else {
+      /**
+       * plugin initialization complete
+       */
+      handleExistingUsers(hoodie, callback);
+    }
+  });
+
   /**
    * plugin initialization complete
    */
-  handleExistingUsers(hoodie, callback);
+  //handleExistingUsers(hoodie, callback);
 
+};
+
+// create /_users/_design/hoodie%2fplugin%2fusers
+// with _view/by-name
+exports.create_user_index = function create_user_index(hoodie, callback) {
+  var users_db = hoodie.database('_users');
+  var index_name = 'by-name';
+
+  var mapReduce = {
+    /* jshint ignore:start */
+    map: function (doc) {
+      var name = doc.name.split('/')[1];
+      var state;
+      if(doc.roles.indexOf('error') !== -1){
+        state = 'error';
+      } else {
+        state = doc.roles.indexOf('confirmed') === -1 ? 'unconfirmed' : 'confirmed';
+      }
+
+      var result = {
+        id: doc.id,
+        name: name,
+        createdAt: doc.createdAt,
+        state: state
+      }
+      emit(name, result);
+    }
+    /* jshint ignore:end */
+  };
+  users_db.addIndex(index_name, mapReduce, callback);
+};
+
+// create /_users/_design/hoodie%2fplugin%2fusers
+// with _view/by-created-at
+exports.create_user_index_by_created_at = function create_user_index_by_created_at(hoodie, callback) {
+  var users_db = hoodie.database('_users');
+  var index_name = 'by-created-at';
+
+  var mapReduce = {
+    /* jshint ignore:start */
+    map: function (doc) {
+      var name = doc.name.split('/')[1];
+      var state;
+      if(doc.roles.indexOf('error') !== -1){
+         state = 'error';
+      } else {
+         state = doc.roles.indexOf('confirmed') === -1 ? 'unconfirmed' : 'confirmed';
+      }
+
+      var result = {
+        id: doc.id,
+        name: name,
+        createdAt: doc.createdAt,
+        state: state
+      }
+      emit(doc.createdAt, result);
+    }
+    /* jshint ignore:end */
+  };
+  users_db.addIndex(index_name, mapReduce, callback);
 };
