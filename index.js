0a20a3c23c2a64fd06f2a54c3cc7c71378976b78
Stephan Bönnemann <stephan@boennemann.me>
2015-11-03 17:14:05 +0100

    style: standard style

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
+/* global emit */ // couchdb globals
+
 /**
  * Users plugin
  * Manages accounts and user dbs
  */
 
-var removeAccount = require('./lib/remove_account');
-var passwordReset = require('./lib/password_reset');
-var changeUsername = require('./lib/change_username');
-var signUp = require('./lib/signup');
-var utils = require('./lib/utils');
-var async = require('async');
+var async = require('async')
 
-var sources = [];
+var changeUsername = require('./lib/change_username')
+var passwordReset = require('./lib/password_reset')
+var removeAccount = require('./lib/remove_account')
+var signUp = require('./lib/signup')
+var utils = require('./lib/utils')
+
+var sources = []
+
+/**
+* Adds a database to the pool of task event sources
+* Makes sure it only happens once per database
+* Maybe move the dedupe logic to hoodie-plugins-manager
+*/
 
 module.exports = function (hoodie, callback) {
+  var addSource = function addSource (doc) {
+    var dbName = utils.userDB(doc)
 
-  /**
-  * Adds a database to the pool of task event sources
-  * Makes sure it only happens once per database
-  * Maybe move the dedupe logic to hoodie-plugins-manager
-  */
-  var addSource = function addSource(doc) {
-    var dbName = utils.userDB(doc);
-
-    if(sources.indexOf(dbName) === -1) {
+    if (sources.indexOf(dbName) === -1) {
       // not a source yet
-      hoodie.task.addSource(dbName);
-      sources.push(dbName);
+      hoodie.task.addSource(dbName)
+      sources.push(dbName)
     }
-  };
+  }
 
   /**
    * Event handlers
    */
-  function userChange(doc) {
+  function userChange (doc) {
     if (doc.$error) {
       // don't do any further processing to user docs with $error
-      return;
+      return
     }
-    else if (doc._deleted && !doc.$newUsername) {
-      removeAccount(hoodie, doc);
+    if (doc._deleted && !doc.$newUsername) {
+      return removeAccount(hoodie, doc)
     }
-    else if (doc.$newUsername) {
-      changeUsername(hoodie, doc);
+    if (doc.$newUsername) {
+      return changeUsername(hoodie, doc)
     }
-    else if (!signUp.isConfirmed(doc) && !signUp.isUnconfirmed(doc)) {
-      signUp(hoodie, doc);
-    } else {
-      addSource(doc);
+    if (!signUp.isConfirmed(doc) && !signUp.isUnconfirmed(doc)) {
+      return signUp(hoodie, doc)
     }
+    return addSource(doc)
   }
 
   /**
@@ -60,61 +62,56 @@ module.exports = function (hoodie, callback) {
     // bootstrap existing users
     hoodie.account.findAll(function (error, accounts) {
       if (error) {
-        console.log('hoodie-plugin-users: can’t bootstrap existing accounts');
-        return done(error);
-      } else {
-        accounts.forEach(userChange);
-        done();
+        console.log('hoodie-plugin-users: can’t bootstrap existing accounts')
+        return done(error)
       }
-    });
+      accounts.forEach(userChange)
+      done()
+    })
   }
 
-
-  hoodie.account.on('user:change', userChange);
-  hoodie.account.on('user_anonymous:change', userChange);
+  hoodie.account.on('user:change', userChange)
+  hoodie.account.on('user_anonymous:change', userChange)
 
   hoodie.account.on('$passwordReset:change', function (doc) {
     if (!doc._deleted && !doc.$error) {
-      passwordReset(hoodie, doc);
+      passwordReset(hoodie, doc)
     }
-  });
+  })
 
   async.applyEachSeries([
     exports.create_user_index,
     exports.create_user_index_by_created_at
-  ], hoodie, function(error) {
-    if(error){
-      return callback(error);
-    } else {
-      /**
-       * plugin initialization complete
-       */
-      handleExistingUsers(hoodie, callback);
+  ], hoodie, function (error) {
+    if (error) {
+      return callback(error)
     }
-  });
+    /**
+     * plugin initialization complete
+     */
+    handleExistingUsers(hoodie, callback)
+  })
 
   /**
    * plugin initialization complete
    */
-  //handleExistingUsers(hoodie, callback);
-
-};
+  // handleExistingUsers(hoodie, callback)
+}
 
 // create /_users/_design/hoodie%2fplugin%2fusers
 // with _view/by-name
-exports.create_user_index = function create_user_index(hoodie, callback) {
-  var users_db = hoodie.database('_users');
-  var index_name = 'by-name';
+exports.create_user_index = function create_user_index (hoodie, callback) {
+  var users_db = hoodie.database('_users')
+  var index_name = 'by-name'
 
   var mapReduce = {
-    /* jshint ignore:start */
     map: function (doc) {
-      var name = doc.name.split('/')[1];
-      var state;
-      if(doc.roles.indexOf('error') !== -1){
-        state = 'error';
+      var name = doc.name.split('/')[1]
+      var state
+      if (doc.roles.indexOf('error') !== -1) {
+        state = 'error'
       } else {
-        state = doc.roles.indexOf('confirmed') === -1 ? 'unconfirmed' : 'confirmed';
+        state = doc.roles.indexOf('confirmed') === -1 ? 'unconfirmed' : 'confirmed'
       }
 
       var result = {
@@ -123,28 +120,26 @@ exports.create_user_index = function create_user_index(hoodie, callback) {
         createdAt: doc.createdAt,
         state: state
       }
-      emit(name, result);
+      emit(name, result)
     }
-    /* jshint ignore:end */
-  };
-  users_db.addIndex(index_name, mapReduce, callback);
-};
+  }
+  users_db.addIndex(index_name, mapReduce, callback)
+}
 
 // create /_users/_design/hoodie%2fplugin%2fusers
 // with _view/by-created-at
-exports.create_user_index_by_created_at = function create_user_index_by_created_at(hoodie, callback) {
-  var users_db = hoodie.database('_users');
-  var index_name = 'by-created-at';
+exports.create_user_index_by_created_at = function create_user_index_by_created_at (hoodie, callback) {
+  var users_db = hoodie.database('_users')
+  var index_name = 'by-created-at'
 
   var mapReduce = {
-    /* jshint ignore:start */
     map: function (doc) {
-      var name = doc.name.split('/')[1];
-      var state;
-      if(doc.roles.indexOf('error') !== -1){
-         state = 'error';
+      var name = doc.name.split('/')[1]
+      var state
+      if (doc.roles.indexOf('error') !== -1) {
+        state = 'error'
       } else {
-         state = doc.roles.indexOf('confirmed') === -1 ? 'unconfirmed' : 'confirmed';
+        state = doc.roles.indexOf('confirmed') === -1 ? 'unconfirmed' : 'confirmed'
       }
 
       var result = {
@@ -153,9 +148,8 @@ exports.create_user_index_by_created_at = function create_user_index_by_created_
         createdAt: doc.createdAt,
         state: state
       }
-      emit(doc.createdAt, result);
+      emit(doc.createdAt, result)
     }
-    /* jshint ignore:end */
-  };
-  users_db.addIndex(index_name, mapReduce, callback);
-};
+  }
+  users_db.addIndex(index_name, mapReduce, callback)
+}
