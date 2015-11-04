e533cd77aa405b9f18b05fb5aa72cf02c90505f3
Stephan Bönnemann <stephan@boennemann.me>
2015-11-04 12:03:08 +0100

    refactor: remove duplication and add more structure

--- index.js
+++ index.js
@@ -5,15 +5,8 @@
  * Manages accounts and user dbs
  */
 
+var _ = require('lodash')
 var async = require('async')
-
-var changeUsername = require('./lib/change_username')
-var passwordReset = require('./lib/password_reset')
-var removeAccount = require('./lib/remove_account')
-var signUp = require('./lib/signup')
-var utils = require('./lib/utils')
-
-var sources = []
 
 /**
 * Adds a database to the pool of task event sources
@@ -22,87 +15,38 @@ var sources = []
 */
 
 module.exports = function (hoodie, callback) {
-  var addSource = function addSource (doc) {
-    var dbName = utils.userDB(doc)
+  var userChange = _.partial(require('./lib/user-change'), hoodie)
 
-    if (sources.indexOf(dbName) === -1) {
-      // not a source yet
-      hoodie.task.addSource(dbName)
-      sources.push(dbName)
-    }
-  }
+  hoodie.account.on('user:change', userChange)
+  hoodie.account.on('user_anonymous:change', userChange)
+  hoodie.account.on('$passwordReset:change', _.partial(require('./lib/password_reset'), hoodie))
 
-  /**
-   * Event handlers
-   */
-  function userChange (doc) {
-    if (doc.$error) {
-      // don't do any further processing to user docs with $error
-      return
-    }
-    if (doc._deleted && !doc.$newUsername) {
-      return removeAccount(hoodie, doc)
-    }
-    if (doc.$newUsername) {
-      return changeUsername(hoodie, doc)
-    }
-    if (!signUp.isConfirmed(doc) && !signUp.isUnconfirmed(doc)) {
-      return signUp(hoodie, doc)
-    }
-    return addSource(doc)
-  }
+  async.series([
+    async.apply(exports.createIndex, hoodie, 'by-name', 'name'),
+    async.apply(exports.createIndex, hoodie, 'by-created-at', 'doc.createdAt')
+  ], function (err) {
+    if (err) return callback(err)
+    /**
+     * Loops through all user accounts and runs them through
+     * the new user procedure. For most users that will mean
+     * that their user database will be added as a listener
+     * to the event system.
+     */
 
-  /**
-   * Loops through all user accounts and runs them through
-   * the new user procedure. For most users that will mean
-   * that their user database will be added as a listener
-   * to the event system.
-   */
-  function handleExistingUsers (hoodie, done) {
     // bootstrap existing users
     hoodie.account.findAll(function (error, accounts) {
       if (error) {
         console.log('hoodie-plugin-users: can’t bootstrap existing accounts')
-        return done(error)
+        return callback(error)
       }
       accounts.forEach(userChange)
-      done()
+      callback()
     })
-  }
-
-  hoodie.account.on('user:change', userChange)
-  hoodie.account.on('user_anonymous:change', userChange)
-
-  hoodie.account.on('$passwordReset:change', function (doc) {
-    if (!doc._deleted && !doc.$error) {
-      passwordReset(hoodie, doc)
-    }
   })
-
-  async.applyEachSeries([
-    exports.create_user_index,
-    exports.create_user_index_by_created_at
-  ], hoodie, function (error) {
-    if (error) {
-      return callback(error)
-    }
-    /**
-     * plugin initialization complete
-     */
-    handleExistingUsers(hoodie, callback)
-  })
-
-  /**
-   * plugin initialization complete
-   */
-  // handleExistingUsers(hoodie, callback)
 }
 
-// create /_users/_design/hoodie%2fplugin%2fusers
-// with _view/by-name
-exports.create_user_index = function create_user_index (hoodie, callback) {
-  var users_db = hoodie.database('_users')
-  var index_name = 'by-name'
+exports.createIndex = function (hoodie, name, toEmit, callback) {
+  var usersDb = hoodie.database('_users')
 
   var mapReduce = {
     map: function (doc) {
@@ -120,36 +64,8 @@ exports.create_user_index = function create_user_index (hoodie, callback) {
         createdAt: doc.createdAt,
         state: state
       }
-      emit(name, result)
-    }
+      emit(toEmit, result)
+    }.toString().replace('toEmit', toEmit)
   }
-  users_db.addIndex(index_name, mapReduce, callback)
-}
-
-// create /_users/_design/hoodie%2fplugin%2fusers
-// with _view/by-created-at
-exports.create_user_index_by_created_at = function create_user_index_by_created_at (hoodie, callback) {
-  var users_db = hoodie.database('_users')
-  var index_name = 'by-created-at'
-
-  var mapReduce = {
-    map: function (doc) {
-      var name = doc.name.split('/')[1]
-      var state
-      if (doc.roles.indexOf('error') !== -1) {
-        state = 'error'
-      } else {
-        state = doc.roles.indexOf('confirmed') === -1 ? 'unconfirmed' : 'confirmed'
-      }
-
-      var result = {
-        id: doc.id,
-        name: name,
-        createdAt: doc.createdAt,
-        state: state
-      }
-      emit(doc.createdAt, result)
-    }
-  }
-  users_db.addIndex(index_name, mapReduce, callback)
+  usersDb.addIndex(name, mapReduce, callback)
 }
